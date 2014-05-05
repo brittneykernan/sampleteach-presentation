d8b756919e3221ac49dbc925bfcbb5f003b0602e
Brittney Kernan <b.kernan@bigspaceship.com>
2014-05-05 14:02:49 -0400

    removed some files, finished presentation

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
 var express = require('express'),
-  //mongoose = require('mongoose'),
   fs = require('fs');
-
-/*var mongoUri = 'mongodb://localhost/noderestpresentation'; 
-mongoose.connect(mongoUri);
-var db = mongoose.connection;
-db.on('error', function () {
-  throw new Error('unable to connect to database at ' + mongoUri);
-});*/
 
 var app = express();
 
@@ -17,9 +9,6 @@ app.configure(function(){
 
 app.use(express.favicon(__dirname + '/public/favicon.ico'));
 app.use(express.static(__dirname + '/public'));
-
-//require('./src/models/musician')
-//require('./src/routes')(app);
 
 app.listen(3000);
 console.log('Listening on port 3000...');
